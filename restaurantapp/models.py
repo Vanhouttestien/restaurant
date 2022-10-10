b8b66edea8e21291d9809b522af74b1c16f4f6a9
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-10-10 14:23:40 +0000

    add costumer and reservation model

--- restaurantapp/models.py
+++ restaurantapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Costumer(models.Model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    phonenumber = models.CharField(max_length=12)
+    email= models.EmailField()
+
+class reservation(models.Model): 
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_people = models.IntegerField()
+    costumer = models.ForeignKey(Costumer, on_delete=models.CASCADE, related_name="costumer")
