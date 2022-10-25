24eccf5feefa9617fee875b24b19a7262c8cddbd
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-10-25 12:26:38 +0000

    change cloudinary serup

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -20,10 +20,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -36,7 +33,7 @@ DEBUG = False
 
 x_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["restaurantapp123.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['restaurantapp123.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -152,13 +149,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
